98cbd5cf186cd2053b5feb3fde2d140f49e01d9f
911neo <jeevankumars1988@gmail.com>
2016-08-12 17:24:42 +0200

    server modules and bootstrap
    server modules and bootstrap

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+app.use('/node_modules',express.static(__dirname+"/node_modules"));
+app.use('/js',express.static(__dirname+"/js"));
+
+app.get('/',function(req,res) {
+  console.log("loading json app");
+
+  res.sendfile('index.html');
+
+});
+
+
+
+
+
+app.listen(3000);
+console.log("server running on port 3000");
